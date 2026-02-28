4a4b6c49af1109ec5f0706a1ddbc0dd8856f966c
FrancoisCollette <francoiscollette07@gmail.com>
2026-02-28 15:13:52 +0100

    Add files via upload

--- SimRayon.py
+++ SimRayon.py
@@ -5,11 +5,11 @@ h =0.5                      # m
 ns = 1.000272983820855
 Ts = 303.15
 Th = 288.15                 # K (15°C)
-def profilTemperatureLin(z):        #tableau ressort T(z), dT/dz
+def profilTemperatureLin(z):        # ressort tableau T(z), dT/dz
     if z < 0:
         return 1, 0
     elif 0 <= z < h:
-        return (Th - Ts)/h *z + Ts, (Th - Ts)/h # T(z) = -15/h=0.5 *z +30
+        return (Th - Ts)/h *z + Ts, (Th - Ts)/h # pente => (Th - Ts)/h 
     else:
         return Th, 0
 
@@ -31,7 +31,7 @@ def trajetRayonEuler(interval, y0, dx, profilTemperature):
     x = [interval[0]]
     
     while x[-1] <= interval[1]:
-        yc = [y[0][-1], y[1][-1]]   #valeurs courantes de y:  [z, i]
+        yc = [y[0][-1], y[1][-1]]   # valeurs courantes de y:  [z, i]
         dy = odefunction(x[-1], yc, profilTemperature)
         
         znext = y[0][-1] + dx*dy[0]
@@ -39,9 +39,9 @@ def trajetRayonEuler(interval, y0, dx, profilTemperature):
             print('le rayon rentre dans le sol')
             break
         inext = y[1][-1] + dx*dy[1]
-        y[0].append(znext) #ajout des nouvelles vals de z et i
+        y[0].append(znext) # ajout des nouvelles vals de z et i
         y[1].append(inext)
-        x.append(x[-1]+dx) #ajout toute dernière val + le pas
+        x.append(x[-1]+dx) # ajout toute dernière val + le pas
     
     return [x, y]
 
@@ -58,22 +58,55 @@ def  trajetRayonIVP(interval, y0, rtol, profilTemperature):
     return [sol.t, sol.y]
 
 
-#faire trajetrayon avec dx entre 10 et 1e-4
-#comparer z (et i?) en x = 1000m entre eurel et ivp (faire différence pour calculer l'erreur)
-#tracer graphe de l'erreur
-
 
 import matplotlib.pyplot as plt
 plt.figure(figsize=(15, 10))
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
+plt.title('Graphique des erreurs en fonction du pas', fontsize=20)
+plt.xlabel('taille du pas (m) (échelle logarithmique)', fontsize=16)
+plt.ylabel('erreur (m)', fontsize=16)
+
+#faire trajetrayon avec dx entre 10 et 1e-4
+#comparer z (et i?) en x = 1000m entre euler et ivp (faire différence pour calculer l'erreur)
+#tracer graphe de l'erreur
+dist = [0, 1000]
+y0 = [1, -0.2*np.pi/180]
+rtol = 1e-10
+solIVP = trajetRayonIVP(dist, y0, rtol, profilTemperatureLin)
+zIVP = solIVP[1][0][-1]
+print('solution par IVP : ', zIVP)
+print('les erreurs en fonction du pas sont :')
+
+pas = [10, 5, 1, 0.5, 0.1, 5e-2, 1e-2, 1e-3, 1e-4]
+erreurTab = []
+for dx in pas:
+    solEuler = trajetRayonEuler(dist, y0, dx, profilTemperatureLin)
+    zEuler = solEuler[1][0][-1]
+    erreur = abs(zEuler - zIVP)
+    erreurTab.append(erreur)
+    print(erreur)
+
+plt.plot(pas, erreurTab, linewidth = 2)
+plt.xscale('log')
+plt.xlim(10, 1e-4)
+
+
+
+plt.figure(figsize=(15, 10))
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.title('Graphique de différents rayons en fonction de i0', fontsize=20)
+plt.xlabel('distance de la source (m)', fontsize=16)
+plt.ylabel('hauteur z du rayon (m)', fontsize=16)
 
 nb_angle = 50
 angle = np.linspace(0.1*np.pi/180, -1*np.pi/180, nb_angle)    #avec angles en radians
 interval = [0, 1000]
 y0 = [1, 0]
-dx = 1                 #///////////////////////////
+dx = 1e-2                 #///////////////////////////  1e-2 s'accompagne d'une erreur de ~1 cm
 for i in angle:
     y0[1] = i
     rslt = trajetRayonEuler(interval, y0, dx, profilTemperatureLin)
     plt.plot(rslt[0], rslt[1][0], linewidth = 2)
+    
