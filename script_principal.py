c862315abd59177097331d746b44f9a68c9361e8
FrancoisCollette <francoiscollette07@gmail.com>
2026-04-03 13:37:00 +0200

    Add files via upload

--- script_principal.py
+++ script_principal.py
@@ -7,12 +7,6 @@ import matplotlib.image as img
 import constantes as c
 
 
-# ----- variable globale pour Q4 ------
-y0 = c.y0
-# nécessaire pour modifier la hauteur initiale de chaque pixel
-
-
-
 # ---------- Question 2.4 ----------
 
 # définit la valeur correcte comme z en xf par IVP
@@ -91,8 +85,8 @@ def parcoursI(profilTemp = sim.profilTemperatureLin):
     plt.ylabel('hauteur du rayon (m)', fontsize=24)
     plt.tick_params(labelsize = 20)
     for i in angle:
-        y0[1] = i
-        x, y = sim.trajetRayonEuler(c.Xinterval, y0, c.dx, profilTemp)
+        c.y0[1] = i
+        x, y = sim.trajetRayonEuler(c.Xinterval, c.y0, c.dx, profilTemp)
         if x[-1] < 1000 :
             plt.plot(x, y[0], color = 'red', linewidth = 1)
             
@@ -108,7 +102,7 @@ def parcoursI(profilTemp = sim.profilTemperatureLin):
 # si le rayon est absorbé par le sol, retourne None
 
 def z(i0, profilTemp = sim.profilTemperatureLin):
-    y0Local = np.array([y0[0], i0])  # prend z0 courant mais sans modifier c.y0
+    y0Local = np.array([c.y0[0], i0])  # prend z0 courant mais sans modifier c.y0
     
     x, y = sim.trajetRayonEuler(c.Xinterval, y0Local, c.dx, profilTemp)
     if x[-1] < c.Xinterval[1] :
@@ -123,7 +117,7 @@ def z(i0, profilTemp = sim.profilTemperatureLin):
 # les rayons absorbés par le sol (z retourne None) entrainent directement l'iteration suivante de la boucle
 # si un seul ou pas de rayons atteint zf alors la fonction retourne des None et statut = -1 ou -2
 
-def imagesMultiples(plot, profilTemp = sim.profilTemperatureLin):
+def imagesMultiples(plot=False, profilTemp = sim.profilTemperatureLin):
     angles = np.linspace(c.iMax, c.iMin, c.nbIbalayage) # de max à min permet d'avoir toujours le rayon direct trouver par bissection dans la première case
     solutions = np.array([None, None])
     j = 0
@@ -151,7 +145,7 @@ def imagesMultiples(plot, profilTemp = sim.profilTemperatureLin):
             if i > 0 : Type = 'direct' ; couleur = 'lightblue'
             elif i < 0 : Type = 'réfracté'; couleur = 'blue'
             
-            x, y = sim.trajetRayonEuler(c.Xinterval, [y0[0], i], c.dx, profilTemp)
+            x, y = sim.trajetRayonEuler(c.Xinterval, [c.y0[0], i], c.dx, profilTemp)
             plt.plot(x, y[0], color=couleur, linewidth = 2, label = f"rayon {Type}")
             
     if plot == True:
@@ -184,7 +178,7 @@ def distAng (plot, profilTemp = sim.profilTemperatureLin):
             angleArrivée[i] = None
             statut -= 1
             continue                                    #break marche aussi car si une seule image, elle est dans la 1e case
-        x, y = sim.trajetRayonEuler(c.Xinterval, [y0[0], i0Particuliers[i] ], c.dx, profilTemp)
+        x, y = sim.trajetRayonEuler(c.Xinterval, [c.y0[0], i0Particuliers[i] ], c.dx, profilTemp)
         angleArrivée[i] = y[1][-1]
     
     distAngu = abs( angleArrivée[0] - angleArrivée[1])
@@ -215,13 +209,17 @@ def distAng (plot, profilTemp = sim.profilTemperatureLin):
 
 # ----- Question 4 -----
 
-# La fonction prend un profil de température comme argument otpionnel, la valeur par défault est sim.profilTemperatureLin
+# Fonction prennant un profil de température comme argument otpionnel, la valeur par défault est sim.profilTemperatureLin
+# Crée un tableau de taille lignesNouvelleImage x largeur initiale de l'image avec lignesNouvelleImage définie dans le module Constantes
+# Pour chaque ligne appelle Euler à l'envers (xf -> 0m) pour déterminé 
+# la hauteur initiale du rayon et copier les pixels de l'image dans le nouveau tableau
+# Termine par afficher l'image vue par l'observateur ainsi qu'un graphe 
+# de la hauteur perçue par l'observateur en fonction de la hauteur réelle de la source
 def imageBacktracing(profilTemp = sim.profilTemperatureLin):
     hIm = c.hauteurImage
     hImObs = c.bornesImageObs
     imageArray = img.imread(c.imgFile)
     shape = np.shape(imageArray)
-    print(shape)
     nbLignes = shape[0]
     nouvelleImageLong = c.lignesNouvelleImage
     nouvelleImage = np.zeros((nouvelleImageLong, shape[1], 3), dtype=np.uint8)
@@ -244,7 +242,7 @@ def imageBacktracing(profilTemp = sim.profilTemperatureLin):
         if z_source < 0 or z_source > hIm:
             continue
             
-        if abs(angle_arrivee - y[1, -1]) <= 1e-12: # compare les angles pour savoir si réfracté 
+        if abs(angle_arrivee - y[1, -1]) <= 1e-6:  # compare les angles pour savoir si réfracté 
             z_sources_direct.append(z_source)      # car hors du gradiant, le rayon va en ligne droite
             z_apps_direct.append(z_app)
         else:
@@ -255,10 +253,10 @@ def imageBacktracing(profilTemp = sim.profilTemperatureLin):
         nouvelleImage[l] = imageArray[ligne_source]
 
     
-    plt.imshow(nouvelleImage)
+    plt.imshow(nouvelleImage, extent=[0, 3, -6, 6])
     print("suppression des extrémités non visibles de l'image ...")
     j=0
-    while( np.all(nouvelleImage[j] == 0) ):         # tronque bande noire de dessus, démarre du haut et supprime chaque bande trouvée jusqu'a ce qu'il y ait de la couleur
+    while( np.all(nouvelleImage[0] == 0) ):         # tronque bande noire de dessus, démarre du haut et supprime chaque bande trouvée jusqu'a ce qu'il y ait de la couleur
         nouvelleImage = nouvelleImage[1 :]
         j +=1
     zmax = z_apps_balayage[j]                       # z_app de la première ligne non noire
@@ -268,21 +266,25 @@ def imageBacktracing(profilTemp = sim.profilTemperatureLin):
         if not np.all(nouvelleImage[i] == 0):
             derniere_ligne = i
     nouvelleImage = nouvelleImage[:derniere_ligne + 1]
-    zmin = z_apps_balayage[j + derniere_ligne] # z_app de la dernière ligne non noire
+    zmin = z_apps_balayage[j + derniere_ligne]      # z_app de la dernière ligne non noire
     
     print('100% - terminé! \n')
-    print("L'image s'étant de ", zmax, 'm à', zmin,' m')
+    print("L'image s'étant de", zmax, 'm à', zmin,'m')
     plt.figure()
-    plt.imshow(imageArray, extent=[0, 3, 0, hIm])
+    plt.imshow(imageArray, extent=[0, 3, 0, hIm], aspect='auto')
     plt.xlabel("largeur (m)", fontsize=14)
     plt.ylabel("altitude (m)", fontsize=14)
-    plt.savefig('image_originaleBis.pdf', dpi=300, bbox_inches='tight' )
+    plt.savefig('image_originaleBis.pdf', dpi=600, bbox_inches='tight' )
 
     plt.figure()
     plt.imshow(nouvelleImage, extent=[0, 3, zmin, zmax])
+    graduations = np.arange(np.ceil(zmin * 2) / 2, np.floor(zmax * 2) / 2 + 0.5, 0.5)
+    graduations = np.concatenate([[zmin], graduations, [zmax]])
+    graduations = np.unique(np.round(graduations, 3))
+    plt.yticks(graduations)
     plt.xlabel("largeur (m)", fontsize=14)
     plt.ylabel("altitude (m)", fontsize=14)
-    plt.savefig('image_modifiéeBis.pdf', dpi=300, bbox_inches='tight')
+    plt.savefig('image_modifiéeBis.pdf', dpi=600, bbox_inches='tight')
     
     plt.figure(figsize=(10, 15))
     plt.plot(z_sources_direct,   z_apps_direct,   'b', linewidth=2, label='rayons directs')
@@ -295,91 +297,11 @@ def imageBacktracing(profilTemp = sim.profilTemperatureLin):
     plt.savefig('graphePositionsVerticalesBis.pdf', dpi=300)
     return 0
 
-# fonction ascociant un indice de ligne de pixels dans l'image en fonction de la hauteur h du rayon en z0
-def hauteurToIndice(h, nbLignes, hIm):
-    if h >= 0:
-        return int( np.round( nbLignes - (h/hIm * nbLignes) ) )
-    elif h < 0:
-        return int( np.round( nbLignes + ((-h)/hIm * nbLignes) ) )
-
-
-# fonction chargeant une image et créant un tableau contenant 2 fois plus de lignes que l'image initiale
-# pour représenter l'image perçue par l'observateur
-# apelle distAng() pour connaitre la hauteur des rayons perçu en x = 0 
-# et hauteurToIndice() pour convertir cette hauteur en indice dans le nouveau tableau
-# copie les lignes de pixels initiales à leurs nouvelles places dans l'image modifiée
-# puis tronque le tableau après la dernière ligne de pixels et affiche l'image
-
-def imageVoiture():
-    hIm = c.hauteurImage # hateur de l'image en (m)
-    zmin = 0
-    imageArray = img.imread(c.imgFile)
-    
-    shape = np.shape(imageArray)
-    nbLignes = shape[0]
-    nouvelleImage = np.zeros( (nbLignes*c.multi, shape[1], 3) , dtype=np.uint8)  # data type assure des valeures entière pour [r,g,b] 
-                                                                           # impossible de convertir tableau en image si r,g,b sont réels
-    plt.figure()
-    plt.imshow(imageArray, extent=[0, 3, zmin, hIm])
-    plt.xlabel("largeur", fontsize=14)
-    plt.ylabel("altitude (m)", fontsize=14)
-    plt.savefig('image_originale.pdf', bbox_inches='tight')
-    
-    Zdirect = np.zeros(nbLignes)
-    Zréfracté = np.zeros(nbLignes)    
-    print('Calcul en cours, cela peut prendre un instant...')
-    for l in range(nbLignes):
-
-        z0 = hIm/nbLignes * l
-        y0[0] = z0
-        z1, z2 = distAng(False)[0, :2]
-        Zdirect[l] = z1
-        Zréfracté[l] = z2
-        if z1 < zmin:
-            zmin = z1
-        elif z2 < zmin:
-            zmin = z2
-        
-        if np.isnan(z1) or np.isnan(z2): # passe à l'itération suivante si aucun rayons n'arrivent à l'observateur
-            continue
-        
-        i1 = hauteurToIndice(z1, nbLignes, hIm)
-        nouvelleImage[i1] = imageArray[nbLignes - l -1]
-        
-        i2 = hauteurToIndice(z2, nbLignes, hIm)
-        nouvelleImage[i2] = imageArray[nbLignes - l -1]
-        
-    print('Terminé !') 
-    derniere_ligne = 0                                  # trouve la dernière ligne qui n'est pas entièrement noire
-    for i in range(nouvelleImage.shape[0]):
-        if not np.all(nouvelleImage[i] == 0):           # test si toute la ligne est différentes de 0,0,0
-            derniere_ligne = i
-    nouvelleImage = nouvelleImage[:derniere_ligne + 1]  # tronque après la dernière ligne non noire
-    
-
-    
-    plt.figure()
-    plt.imshow(nouvelleImage, extent=[0, 3, zmin, hIm])
-    plt.xlabel("largeur", fontsize=14)
-    plt.ylabel("altitude (m)", fontsize=14)
-    plt.savefig('image_modifiée.pdf', bbox_inches='tight')
-    
-    Zinit = np.linspace(0, hIm, nbLignes)
-    plt.figure(figsize=(10,15))
-    plt.plot(Zinit, Zdirect, 'r-', label='positions par rayons directs')
-    plt.plot(Zinit, Zréfracté, 'b-', label='positions par rayons réfractés')
-    plt.xlabel("position verticale de la source", fontsize=14)
-    plt.ylabel("position verticale perçue", fontsize=14)
-    plt.tick_params(labelsize = 20)
-    plt.legend(loc='upper left', fontsize='24')
-    plt.savefig('graphePositionsVerticales.pdf', bbox_inches='tight')
-    return 0
-
 
 # ---- Question bonus -----
 
 # fonction d'aide à la visualisation du phénomène
-def printProfilTemp(profilTemp = sim.profilTemperatureLin):
+def T(profilTemp = sim.profilTemperatureLin):
     ztab = np.linspace(0, c.bornesImageObs[0], 100)
     Ttab = np.zeros(100)
     j = 0
@@ -388,6 +310,6 @@ def printProfilTemp(profilTemp = sim.profilTemperatureLin):
         j +=1
     plt.figure(figsize=(20, 5))
     plt.plot(ztab, Ttab, linewidth=2)
-    plt.xlabel("altitude (m)", fontsize=14)
-    plt.ylabel("température (K)", fontsize=14)
+    plt.xlabel("altitude (m)", fontsize=24)
+    plt.ylabel("température (K)", fontsize=24)
     plt.tick_params(labelsize = 20)
